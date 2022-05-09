76c115ec7c09b50c9536d8a08dc133b472424196
KoreanCodingMachine <pon03078@naver.com>
2022-05-09 23:58:10 +0900

    commit login api

--- server/model/User.js
+++ server/model/User.js
@@ -1,9 +1,16 @@
 const mongoose = require('mongoose');
 const config = require('../config/key');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const saltRounds = 10;
+
 const dbConnect = () => {
   mongoose.connect(config.mongoURI);
 };
-// const ObjectId = Schema.ObjectId;
+var validateEmail = function (email) {
+  var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return re.test(email);
+};
 
 const userSchema = mongoose.Schema({
   name: {
@@ -11,11 +18,20 @@ const userSchema = mongoose.Schema({
     maxlength: 50,
   },
   email: {
-    tpye: String,
+    type: String,
+    trim: true,
+    lowercase: true,
+    unique: true,
+    required: 'Email address is required',
+    validate: [validateEmail, 'Please fill a valid email address'],
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      'Please fill a valid email address',
+    ],
   },
   password: {
     type: String,
-    maxlength: 5,
+    maxlength: 500,
   },
   lastname: {
     type: String,
@@ -34,7 +50,63 @@ const userSchema = mongoose.Schema({
   },
 });
 
+userSchema.pre('save', function (next) {
+  //비밀번호를 암호화 시킨다.
+  var user = this;
+  if (user.isModified('password')) {
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+  //plainpassword 1234567  암호화된 비밀번호 "$2b$10$IMZdKiV0WLpFHHzQPHzFAOns.ueaobGEXLbl2viGMkkj4lyQeuKXa"
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
+userSchema.methods.generateToken = function (cb) {
+  var user = this;
+
+  //jsonwebtoken을 이용해서 token을 생성하기
+  var token = jwt.sign(user._id.toHexString(), 'secretToken');
+  // user._id + 'secretToken' = token
+  // ->
+  // 'secretToken' -> user._id
+
+  user.token = token;
+  user.save(function (err, user) {
+    if (err) return cb(err);
+    cb(null, user);
+  });
+};
+
+userSchema.statics.findByToken = function (token, cb) {
+  var user = this;
+
+  //토큰을 decode 한다.
+  jwt.verify(token, 'secretToken', function (err, decoded) {
+    //유저 아이디를 이용해서 유저를 찾은 이후에
+    //클라이언트에서 가져온 token과 db에서 보관된 토큰이 일치하는지 확인
+
+    user.findOne({ _id: decoded, token: token }, function (err, user) {
+      if (err) return cb(err);
+      cb(null, user);
+    });
+  });
+};
+
 dbConnect();
 const User = mongoose.model('User', userSchema);
-
 module.exports = { User };
