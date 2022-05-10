1e9516d85ec6206c766a1b6503e3b918334ba284
KoreanCodingMachine <pon03078@naver.com>
2022-05-10 14:41:28 +0900

    commit User.js

--- server/app.js
+++ server/app.js
@@ -4,8 +4,20 @@ const { User } = require('./model/User');
 const { auth } = require('./middleware/auth');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const config = require('./config/key');
 const app = express();
 app.use(express.json());
+
+const mongoose = require('mongoose');
+mongoose
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch((err) => console.log(err));
 
 //application / x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -28,7 +40,7 @@ app.post('/api/product', async function (req, res) {
   res.send('success');
 });
 
-app.post('/api/register', function (req, res) {
+app.post('/api/users/register', function (req, res) {
   //회원가입 할때 필요한 정보들을 client에서 가져오면
   //그것들을 데이터 베이스에 넣어준다.
 
@@ -41,7 +53,7 @@ app.post('/api/register', function (req, res) {
   });
 });
 
-app.post('/api/login', function (req, res) {
+app.post('/api/users/login', function (req, res) {
   //요청된 이메일을 데이터 베이스에서 있는지 찾는다.
   User.findOne({ email: req.body.email }, (err, user) => {
     if (!user) {
--- server/model/User.js
+++ server/model/User.js
@@ -1,16 +1,7 @@
 const mongoose = require('mongoose');
-const config = require('../config/key');
 const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
 const saltRounds = 10;
-
-const dbConnect = () => {
-  mongoose.connect(config.mongoURI);
-};
-var validateEmail = function (email) {
-  var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  return re.test(email);
-};
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
   name: {
@@ -20,18 +11,11 @@ const userSchema = mongoose.Schema({
   email: {
     type: String,
     trim: true,
-    lowercase: true,
-    unique: true,
-    required: 'Email address is required',
-    validate: [validateEmail, 'Please fill a valid email address'],
-    match: [
-      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-      'Please fill a valid email address',
-    ],
+    unique: 1,
   },
   password: {
     type: String,
-    maxlength: 500,
+    minlength: 5,
   },
   lastname: {
     type: String,
@@ -43,7 +27,7 @@ const userSchema = mongoose.Schema({
   },
   image: String,
   token: {
-    tpye: String,
+    type: String,
   },
   tokenExp: {
     type: Number,
@@ -51,9 +35,9 @@ const userSchema = mongoose.Schema({
 });
 
 userSchema.pre('save', function (next) {
-  //비밀번호를 암호화 시킨다.
   var user = this;
   if (user.isModified('password')) {
+    //비밀번호를 암호화 시킨다.
     bcrypt.genSalt(saltRounds, function (err, salt) {
       if (err) return next(err);
 
@@ -69,7 +53,7 @@ userSchema.pre('save', function (next) {
 });
 
 userSchema.methods.comparePassword = function (plainPassword, cb) {
-  //plainpassword 1234567  암호화된 비밀번호 "$2b$10$IMZdKiV0WLpFHHzQPHzFAOns.ueaobGEXLbl2viGMkkj4lyQeuKXa"
+  //plainPassword 1234567    암호회된 비밀번호 $2b$10$l492vQ0M4s9YUBfwYkkaZOgWHExahjWC
   bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
     if (err) return cb(err);
     cb(null, isMatch);
@@ -78,8 +62,9 @@ userSchema.methods.comparePassword = function (plainPassword, cb) {
 
 userSchema.methods.generateToken = function (cb) {
   var user = this;
+  // console.log('user._id', user._id)
 
-  //jsonwebtoken을 이용해서 token을 생성하기
+  // jsonwebtoken을 이용해서 token을 생성하기
   var token = jwt.sign(user._id.toHexString(), 'secretToken');
   // user._id + 'secretToken' = token
   // ->
@@ -94,12 +79,11 @@ userSchema.methods.generateToken = function (cb) {
 
 userSchema.statics.findByToken = function (token, cb) {
   var user = this;
-
+  // user._id + ''  = token
   //토큰을 decode 한다.
   jwt.verify(token, 'secretToken', function (err, decoded) {
-    //유저 아이디를 이용해서 유저를 찾은 이후에
-    //클라이언트에서 가져온 token과 db에서 보관된 토큰이 일치하는지 확인
-
+    //유저 아이디를 이용해서 유저를 찾은 다음에
+    //클라이언트에서 가져온 token과 DB에 보관된 토큰이 일치하는지 확인
     user.findOne({ _id: decoded, token: token }, function (err, user) {
       if (err) return cb(err);
       cb(null, user);
@@ -107,6 +91,6 @@ userSchema.statics.findByToken = function (token, cb) {
   });
 };
 
-dbConnect();
 const User = mongoose.model('User', userSchema);
+
 module.exports = { User };
